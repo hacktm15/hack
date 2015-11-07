a3f03bdf975294bf87d010de085f37db7b0b4799
boco4ever <boco4ever@yahoo.com>
2015-11-07 11:45:27 +0200

    succefuly returned the json result

--- FestivalGatherer/FestivalGatherer/Controllers/GatherController.cs
+++ FestivalGatherer/FestivalGatherer/Controllers/GatherController.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration.Provider;
 using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Mvc;
+using FestivalGatherer.DataProviders;
 
 namespace FestivalGatherer.Controllers
 {
@@ -52,27 +50,5 @@ namespace FestivalGatherer.Controllers
         //public void Delete(int id)
         //{
         //}
-    }
-
-    public class Provider
-    {
-        public static void QuerryFestivals()
-        {
-            string requestUrl = HmcSha1.Hash2("qXVl3oyM75QhQtz4", "MWR6cePblwXxSg38vqAzbJ2rg6u6ykcz", "year=2015");
-            var request = (HttpWebRequest) WebRequest.Create(requestUrl);
-            request.Accept="application/json;ver=2.0";
-            using (var response = (HttpWebResponse) request.GetResponse())
-            {
-                Stream receiveStream = response.GetResponseStream();
-                if (receiveStream == null)
-                {
-                    return;
-                }
-                using (var readStream = new StreamReader(receiveStream, Encoding.UTF8))
-                {
-                    var x = readStream.ReadToEnd();
-                }
-            }
-        }
     }
 }
---
+++ FestivalGatherer/FestivalGatherer/DataProviders/Provider.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Web.Helpers;
+using FestivalGatherer.Controllers;
+
+namespace FestivalGatherer.DataProviders
+{
+    public class Provider
+    {
+        public static void QuerryFestivals()
+        {
+            string requestUrl = HmcSha1.Hash2("qXVl3oyM75QhQtz4", "MWR6cePblwXxSg38vqAzbJ2rg6u6ykcz", "year=2015");
+            var request = (HttpWebRequest) WebRequest.Create(requestUrl);
+            request.Accept="application/json;ver=2.0";
+            using (var response = (HttpWebResponse) request.GetResponse())
+            {
+                Stream receiveStream = response.GetResponseStream();
+                if (receiveStream == null)
+                {
+                    return;
+                }
+                using (var readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    var streamResponse = readStream.ReadToEnd();
+                    var festivals = Json.Decode(streamResponse);
+                }
+            }
+        }
+    }
+}
